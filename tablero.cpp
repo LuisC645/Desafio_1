c7cc70c69667130959bd1c46c24114cecba7816f
luisc645 <luis.castilloch645@gmail.com>
2026-09-10 16:42:34 -0500

    set y get fichas

--- tablero.cpp
+++ tablero.cpp
@@ -20,3 +20,48 @@ unsigned char* crearTablero(unsigned short filas, unsigned short columnas){
 void elimnarTablero(unsigned char* tablero){
     delete[] tablero;
 }
+
+
+unsigned char leerFicha(unsigned char* tablero, unsigned short fila, unsigned short columna, unsigned short columnas){
+    unsigned short indice = (fila * columnas) + columna; // El paqt n del bit de interes
+    unsigned short bitInicial = indice * 3; // El bit de interes (pos inicial)
+
+    unsigned short byteIndice = bitInicial / 8; // Byte donde se encuentra la ficha
+    unsigned short offset = bitInicial % 8; // Pos del bit en el byte de interes
+
+    unsigned short ventana = tablero[byteIndice];
+
+    if(offset > 5){
+        ventana |= (tablero[byteIndice + 1] << 8);
+    }
+
+    return (ventana >> offset) & 0x07;
+}
+
+void escribirFicha(unsigned char* tablero, unsigned short fila, unsigned short columna, unsigned short columnas, unsigned char valor){
+    unsigned short indice = (fila * columnas) + columna; // El paqt n del bit de interes
+    unsigned short bitInicial = indice * 3; // El bit de interes (pos inicial)
+
+    unsigned short byteIndice = bitInicial / 8; // Byte donde se encuentra la ficha
+    unsigned short offset = bitInicial % 8; // Pos del bit en el byte de interes
+
+    unsigned short ventana = tablero[byteIndice];
+
+    if(offset > 5){
+        ventana |= (tablero[byteIndice + 1] << 8);
+    }
+
+    // Poner 0 la ficha actual
+    ventana &= ~(0x07 << offset);
+
+    // Colocar el valor
+    ventana |= ((valor & 0x07) << offset);
+
+    // poner ventana en tablero
+    tablero[byteIndice] = ventana & 0xFF;
+
+    if(offset > 5){
+        tablero[byteIndice + 1] = (ventana >> 8) & 0xFF;
+    }
+}
+
--- tablero.h
+++ tablero.h
@@ -5,4 +5,8 @@ unsigned short bytesNecesarios(unsigned short filas, unsigned short columnas);
 unsigned char* crearTablero(unsigned short filas, unsigned short columnas);
 void eliminarTablero(unsigned char* tablero);
 
+// getters - setters
+unsigned char leerFicha(unsigned char* tablero, unsigned short fila, unsigned short columna, unsigned short columnas);
+void escribirFicha(unsigned char* tablero, unsigned short fila, unsigned short columna, unsigned short columnas, unsigned char valor);
+
 #endif // TABLERO_H
